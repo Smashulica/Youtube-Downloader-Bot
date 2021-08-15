df4d5c557dd185b4f6bae7876cc03231aaf3e9d7
Smashulica <85994708+Smashulica@users.noreply.github.com>
2021-08-15 04:06:33 +0100

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"<b>💁🏻‍♂️ How To Use Me?</b>\n\n<b>Doar trimite un link de YouTube si eu voi descarca pentru tine fisierul video sau MP3 !</b>\n<b>(Playlist sau inline inca nu este suportat ☹️)</b>"
+    helptxt = f"<b>💁🏻‍♂️ Cum pot fi folosit?</b>\n\n<b>Doar trimite un link de YouTube si eu voi descarca pentru tine fisierul video sau MP3 !</b>\n<b>(Playlist sau inline inca nu este suportat ☹️)</b>\n\n<b>Dar merg cu @vid 🤘🏻</b>"
     await message.reply_text(helptxt)

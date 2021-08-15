79369b9f0c500afbb39293c88c133be6be48fb93
Smashulica <85994708+Smashulica@users.noreply.github.com>
2021-08-15 03:55:37 +0100

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
+    helptxt = f"<b>💁🏻‍♂️ How To Use Me?</b>\n\n<b>Doar trimite un link de YouTube si eu voi descarca pentru tine fisierul video sau MP3 !</b>\n<b>(Playlist sau inline inca nu este suportat ☹️)</b>"
     await message.reply_text(helptxt)
